29c3a3ec61f24e4126d48bd501796e3f0effb633
Antropov Andrey <q.laiff@gmail.com>
2020-02-20 16:28:00 +0500

    feat: improve logging output

--- src/logger.ts
+++ src/logger.ts
@@ -1,46 +1,102 @@
 import debounce from 'just-debounce-it';
-import { Store } from 'effector';
+import { Effect, Event, Store } from 'effector';
 import { createName, getPath } from './lib';
 
 const storeListToInit: Array<Store<any>> = [];
+const eventListToInit: Array<Event<any>> = [];
+const effectListToInit: Array<Effect<any, any, any>> = [];
 
-const logStore = debounce(() => {
-  const list = storeListToInit.splice(0);
-  if (list.length > 0) {
+const firstLabel = (bgColor: string, color: string) =>
+  `background-color:${bgColor}; color:${color}; padding: 0 4px; border-radius: 4px 0 0 4px`;
+const nextLabel = (bgColor: string, color: string) =>
+  `background-color:${bgColor}; color:${color}; padding: 0 4px;`;
+
+const logAdded = debounce(() => {
+  const stores = storeListToInit.splice(0);
+  const events = eventListToInit.splice(0);
+  const effects = effectListToInit.splice(0);
+
+  if (stores.length > 0 || events.length > 0 || effects.length > 0) {
     console.groupCollapsed(
-      `[effector-logger] Initialized stores (${list.length})`,
+      `%c${"☄"}%c${"new"}%c Initialized events (${events.length}) effects (${effects.length}) stores (${stores.length})`,
+      firstLabel("#ff8a65", "#000"),
+      nextLabel("#29b6f6", "#000"),
+      nextLabel("transparent", "currentColor"),
     );
-    for (const store of list) {
+    stores.length > 0 && stores.forEach(store => {
       const name = createName(store.compositeName);
       const fileName = getPath(store);
 
-      console.log(
-        '[effector-logger] %cSTORE%c %s VALUE(%o) %c%s',
-        'color: deepskyblue;',
-        'color: currentColor;',
+      console.info(
+        `%c${"☄"}%c${"new"}%c${"store"}%c%s->%o %c%s`,
+        firstLabel("#ff8a65", "#000"),
+        nextLabel("#29b6f6", "#000"),
+        nextLabel("#7e57c2", "#fff"),
+        nextLabel("transparent", "currentColor"),
         name,
         store.defaultState,
-        'color: gray',
+        nextLabel("transparent", "#9e9e9e"),
         fileName,
       );
-    }
+    })
+    events.length > 0 && events.forEach(event => {
+      const name = createName(event.compositeName);
+      const fileName = getPath(event);
+
+      console.info(
+        `%c${"☄"}%c${"new"}%c${"event"}%c%s %c%s`,
+        firstLabel("#ff8a65", "#000"),
+        nextLabel("#29b6f6", "#000"),
+        nextLabel("#9ccc65", "#000"),
+        nextLabel("transparent", "currentColor"),
+        name,
+        nextLabel("transparent", "#9e9e9e"),
+        fileName,
+      );
+    })
+    effects.length > 0 && stores.forEach(effect => {
+      const name = createName(effect.compositeName);
+      const fileName = getPath(effect);
+
+      console.info(
+        `%c${"☄"}%c${"new"}%c${"effect"}%c%s %c%s`,
+        firstLabel("#ff8a65", "#000"),
+        nextLabel("#29b6f6", "#000"),
+        nextLabel("#26a69a", "#000"),
+        nextLabel("transparent", "currentColor"),
+        name,
+        nextLabel("transparent", "#9e9e9e"),
+        fileName,
+      );
+    })
     console.groupEnd();
   }
 }, 5);
 
 export function storeAdded(store: Store<any>): void {
   storeListToInit.push(store);
-  logStore();
+  logAdded();
+}
+
+export function eventAdded(event: Event<any>): void {
+  eventListToInit.push(event);
+  logAdded();
+}
+
+export function effectAdded(effect: Effect<any, any, any>): void {
+  effectListToInit.push(effect);
+  logAdded();
 }
 
 export function storeUpdated(name: string, fileName: string, value: any): void {
-  console.log(
-    '[effector-logger] %cSTORE%c %s VALUE(%o) %c%s',
-    'color: deepskyblue;',
-    'color: currentColor;',
+  console.info(
+    `%c${"☄"}%c${"store"}%c%s->%o %c%s`,
+    firstLabel("#ff8a65", "#000"),
+    nextLabel("#7e57c2", "#fff"),
+    nextLabel("transparent", "currentColor"),
     name,
     value,
-    'color: gray',
+    nextLabel("transparent", "#9e9e9e"),
     fileName,
   );
 }
@@ -51,12 +107,13 @@ export function eventCalled(
   payload: any,
 ): void {
   console.log(
-    '[effector-logger] %cEVENT%c %s PAYLOAD(%O) %c%s',
-    'color: magenta;',
-    'color: currentColor;',
+    `%c${"☄"}%c${"event"}%c%s->%o %c%s`,
+    firstLabel("#ff8a65", "#000"),
+    nextLabel("#9ccc65", "#000"),
+    nextLabel("transparent", "currentColor"),
     name,
     payload,
-    'color: gray;',
+    nextLabel("transparent", "#9e9e9e"),
     fileName,
   );
 }
@@ -67,12 +124,13 @@ export function effectCalled(
   parameters: any,
 ): void {
   console.log(
-    '[effector-logger] %cEFFECT%c %s PARAMS(%o) %c%s',
-    'color: orange;',
-    'color: currentColor;',
+    `%c${"☄"}%c${"effect"}%c%s->%o %c%s`,
+    firstLabel("#ff8a65", "#000"),
+    nextLabel("#26a69a", "#000"),
+    nextLabel("transparent", "currentColor"),
     name,
     parameters,
-    'color: gray',
+    nextLabel("transparent", "#9e9e9e"),
     fileName,
   );
 }
@@ -84,13 +142,14 @@ export function effectDone(
   result: any,
 ): void {
   console.log(
-    '[effector-logger] %cEFFECT DONE%c %s PARAMS(%o) -> %o %c%s',
-    'color: green;',
-    'color: currentColor;',
+    `%c${"☄"}%c${"effect"}%c${"D"}%c%s->%o %c%s`,
+    firstLabel("#ff8a65", "#000"),
+    nextLabel("#26a69a", "#000"),
+    nextLabel("#66bb6a", "#000"),
+    nextLabel("transparent", "currentColor"),
     name,
     parameters,
-    result,
-    'color: gray',
+    nextLabel("transparent", "#9e9e9e"),
     fileName,
   );
 }
@@ -102,13 +161,13 @@ export function effectFail(
   error: any,
 ): void {
   console.log(
-    '[effector-logger] %cEFFECT FAIL%c %s PARAMS(%o) -> %o %c%s',
-    'color: red;',
-    'color: currentColor;',
+    `%c${"☄"}%c${"effect"}%c${"F"}%c%s->%o %c%s`,
+    firstLabel("#ff8a65", "#000"),
+    nextLabel("#26a69a", "#000"),
+    nextLabel("#ef5350", "#000"),
+    nextLabel("transparent", "currentColor"),
     name,
     parameters,
-    error,
-    'color: gray',
-    fileName,
-  );
+    nextLabel("transparent", "#9e9e9e"),
+    fileName,  );
 }
