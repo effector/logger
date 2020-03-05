b94a0a070941ceda9c41434a437fdff8ac211d91
Antropov Andrey <q.laiff@gmail.com>
2020-03-05 16:50:45 +0500

    fix: update usage for new api

--- usage/api/session.ts
+++ usage/api/session.ts
@@ -3,7 +3,6 @@ import { createDomain } from '../../src';
 const api = createDomain();
 
 export const sessionFetch = api
-  .createEffect<number, null | { id: number }>()
-  .use((params) => {
-    return { id: 100000 + params };
+  .createEffect<number, null | { id: number }>({
+    handler: params => ({ id: 100000 + params }),
   });
