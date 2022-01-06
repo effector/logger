50d2715236e1b32371b5b4d7ca5e511469390548
Alexander Khoroshikh <32790736+AlexandrHoroshih@users.noreply.github.com>
2022-01-06 14:11:02 +0300

    test(integration/custom): added test for create-inspector

--- integration/custom/src/logger.test.ts
+++ integration/custom/src/logger.test.ts
@@ -1,7 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-
 import { $counter, thingHappened } from './model';
 
 declare global {
@@ -12,24 +11,88 @@ declare global {
   }
 }
 
-test('console logger is initialized', async () => {
-  const log = jest.spyOn(console, 'log').mockImplementation(() => null);
+const info = jest.spyOn(console, 'info');
+const log = jest.spyOn(console, 'log');
+const group = jest.spyOn(console, 'groupCollapsed');
+const reduxDevToolsSend = jest.spyOn(window.__REDUX_DEVTOOLS_EXTENSION__, 'send');
 
-  expect($counter.sid).toMatchInlineSnapshot(`"evspfo"`);
-  expect(thingHappened.sid).toMatchInlineSnapshot(`"-vjdci1"`);
+test('console logger is initialized', async () => {
+  expect($counter.sid).toMatchInlineSnapshot(`"fcubtv"`);
+  expect(thingHappened.sid).toMatchInlineSnapshot(`"-v2bq3u"`);
   await new Promise((r) => setTimeout(r));
-  expect(log.mock.calls.map((call) => call[0])).toMatchInlineSnapshot(`
+  expect(log.mock.calls).toMatchInlineSnapshot(`
     Array [
-      "%c%s%c  %c%s%c  %c%s%c  %c%s%c  %c%s  %c%o  %c%s  %c%s",
-      "%c%s%c  %c%s%c  %c%s%c  %c%s%c  %c%s  %c%s",
+      Array [
+        "%c%s%c  %c%s%c  %c%s%c  %c%s%c  %c%s  %c%o  %c%s  %c%s",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "☄️",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "effector",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #29b6f6; color: #000",
+        "new",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #29b6f6; color: #000",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #7e57c2; color: #fff",
+        "$counter",
+        "color: currentColor; background-color: transparent;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "-> ",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        0,
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; color: #9e9e9e; padding-left: 20px;",
+        "/src/model.ts",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "$counter",
+      ],
+      Array [
+        "%c%s%c  %c%s%c  %c%s%c  %c%s%c  %c%s  %c%s",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "☄️",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "effector",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #29b6f6; color: #000",
+        "new",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #29b6f6; color: #000",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #9ccc65; color: #000",
+        "thingHappened",
+        "color: currentColor; background-color: transparent;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; color: #9e9e9e; padding-left: 20px;",
+        "/src/model.ts",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "thingHappened",
+      ],
     ]
   `);
-
-  log.mockRestore();
+  expect(group.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        "%c%s%c  %c%s%c  %c%s%c  %c%s  %c%s  %c%s  %c%s",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "☄️",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; line-height:1.5; color: #000; font-family: \\"Apple Emoji Font\\"; font-weight: normal !important;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "effector",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; font-family: Menlo, monospace;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; background-color: #29b6f6; color: #000",
+        "new",
+        "color: currentColor; background-color: transparent;",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "Initialized",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "events(1)",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "effects(0)",
+        "padding-left: 4px; padding-right: 4px; font-weight: normal; ",
+        "stores(1)",
+      ],
+    ]
+  `);
 });
 
 test('redux-devtools initialized', async () => {
-  const reduxDevToolsSend = jest.spyOn(window.__REDUX_DEVTOOLS_EXTENSION__, 'send');
   thingHappened();
 
   await new Promise((r) => setTimeout(r));
@@ -46,4 +109,21 @@ test('redux-devtools initialized', async () => {
       },
     ]
   `);
+});
+
+test('inspector initialized', async () => {
+  await new Promise((r) => setTimeout(r));
+
+  expect(info.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "%c[effector-inspector] %cPress %cCTRL+B %cto open Inspector",
+          "color: gray; font-size: 1rem;",
+          "color: currentColor; font-size: 1rem;",
+          "color: deepskyblue; font-family: monospace; font-size: 1rem;",
+          "color: currentColor; font-size: 1rem;",
+        ],
+      ]
+  `);
+  expect(document.querySelector('.effector-inspector')).toBeTruthy()
 });
--- integration/custom/src/model.ts
+++ integration/custom/src/model.ts
@@ -1,4 +1,9 @@
 import {createStore, createEvent } from "effector";
+import { createInspector } from 'effector-inspector';
 
 export const thingHappened = createEvent()
 export const $counter = createStore(0).on(thingHappened, s => s + 1)
+
+// otherwise console.info fires to early, before jest can mock it :(
+// didn't found any good way to test it better
+setTimeout(createInspector);
