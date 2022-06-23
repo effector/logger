1b41ade684f9af5b0a773315b69293e3c8b7b162
Victor Didenko <victord@setplex.com>
2022-06-23 22:15:20 +0300

    feat: add possibility to disable effector-logger for exact import

--- babel-plugin.js
+++ babel-plugin.js
@@ -5,12 +5,24 @@ module.exports = function effectorLogger(babel, options = {}) {
   const { inspector = false, effector = {}, skipEffectorPlugin = false } = options;
 
   const replaceToLogger = ['effector'];
+  const disableRe = /^\s*effector-logger:\s*disable\s*.*$/
+
+  const isLoggerDisabled = (node) => {
+    if (node.trailingComments) {
+      for (const comment of node.trailingComments) {
+        if (comment && disableRe.test(comment.value)) {
+          return true
+        }
+      }
+    }
+  }
 
   const importVisitor = {
     ImportDeclaration(path, state) {
       if (t.isLiteral(path.node.source)) {
         // If imported from module that should be replaced to logger as is
-        if (replaceToLogger.includes(path.node.source.value)) {
+        // and does not contain trailing comment `effector-logger: disable`
+        if (replaceToLogger.includes(path.node.source.value) && !isLoggerDisabled(path.node)) {
           path.node.source.value = 'effector-logger';
         }
       }
