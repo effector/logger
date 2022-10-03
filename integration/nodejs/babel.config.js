435e46851375b8f015f2adeb07087c7e58da4776
Sergey Sova <mail@sergeysova.com>
2022-10-03 17:08:10 +0400

    test: disable inspector on nodejs

--- integration/nodejs/babel.config.js
+++ integration/nodejs/babel.config.js
@@ -1,9 +1,11 @@
 const config = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  plugins: [
+    'effector-logger/babel-plugin',
+    {
+      inspector: false,
+    },
   ],
-  plugins: ['effector-logger/babel-plugin'],
 };
 
 module.exports = config;
