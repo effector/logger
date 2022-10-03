8b4b0348edbfc7e97dbbccb1ecd046fd3208e520
Sergey Sova <mail@sergeysova.com>
2022-10-03 17:11:01 +0400

    test: fix plugin definition

--- integration/nodejs/babel.config.js
+++ integration/nodejs/babel.config.js
@@ -1,11 +1,6 @@
 const config = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-  plugins: [
-    'effector-logger/babel-plugin',
-    {
-      inspector: false,
-    },
-  ],
+  plugins: [['effector-logger/babel-plugin', { inspector: false }]],
 };
 
 module.exports = config;
